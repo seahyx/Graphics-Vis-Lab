736ce4dbcf46fdf16e46b26ac28de08f87a5e905
Seah Ying Xiang <seahyx@gmail.com>
2025-02-20 13:03:51 +0800

    Added some shader shit

--- Mandelbrot/src/main.cpp
+++ Mandelbrot/src/main.cpp
@@ -88,18 +88,31 @@ int main()
 	glViewport(0, 0, 800, 600);
 	/* Register Callbacks */
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+	
+	last_time = glfwGetTime(); // Initialize last_time for FPS counting
 
 	
-	// Setup triangle and vertices
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
-	};
-	unsigned int VBO;
+	// Setup vertex data and buffers and configure vertex attributes
+	unsigned int VAO, VBO, EBO;
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	// Bind VAO
+	glBindVertexArray(VAO);
+
+	// Bind VBO and copy vertices into buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 
 	///////////////////////////
@@ -110,12 +123,16 @@ int main()
 		// Process key inputs
 		processInput(window);
 
+		countFPS();
+
 		// Core rendering commands
-		
 		// Clear buffer with specified background color
 		glClearColor(.2f, .3f, .3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// Check and call events and swap buffers
 		glfwSwapBuffers(window);
@@ -126,6 +143,10 @@ int main()
 	///////////////////////////
 
 	/* Cleanup */
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
 	glfwTerminate();
 
 	return 0;
